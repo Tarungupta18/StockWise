7a57808acb11b5c48958b708684e3edacae856c9
tarun gupta <tkg21430@gmail.com>
2024-09-19 22:40:17 +0530

    pricing-component

--- frontend/src/landing_page/pricing/Brojkerege.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Brojkerege() {
-  return (
-    <div>Brojkerege</div>
-  )
-}
--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import Hero from "./Hero";
+import Brokerage from "./Brokerage";
+import OpenAccount from "../OpenAccount";
 
 export default function PricingPage() {
   return (
-    <div>PricingPage</div>
+    <>
+      <Hero />
+      <OpenAccount />
+      <Brokerage />
+    </>
   )
 }
